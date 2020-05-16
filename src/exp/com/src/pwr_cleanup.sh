0bc19cd61b323ede3819e64a9b439a20de662275
houkime <qahmnatorA_Tprotonmail.com>
2020-05-16 19:07:40 -0400

    cleanup.sh adapted

--- src/exp/com/src/pwr_cleanup.sh
+++ src/exp/com/src/pwr_cleanup.sh
@@ -50,7 +50,11 @@ eval `ipcs -q|grep ^0x|grep "[ \t]$USER[ \t]"|awk '{printf "ipcrm msg %s;", $2}'
 # remove shared memory
 eval `ipcs -m|grep ^0x|grep "[ \t]$USER[ \t]"|awk '{printf "ipcrm shm %s;", $2}'`
 
-if [ $TMPDIR ]; then 
-	rm $TMPDIR/pwr*$PWR_BUS_ID
+tmpdir='/tmp/'
+
+if [ $TMPDIR ]; 
+then 
+	tmpdir=$TMPDIR
 fi
-rm /tmp/pwr*$PWR_BUS_ID
+
+rm $tmpdir/pwr*$PWR_BUS_ID
